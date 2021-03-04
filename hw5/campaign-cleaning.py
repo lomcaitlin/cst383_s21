b3328d4137396e1ec0b2a821ca2ee6eba5db46a4
lomcaitlin <lomcaitlin@gmail.com>
2021-03-04 14:34:29 -0800

    complete wk5 hw

--- hw5/campaign-cleaning.py
+++ hw5/campaign-cleaning.py
@@ -46,25 +46,24 @@ df.info()
 #@ 1
 # What is the total number of NAs in df?
 # (compute a number)
-
+df.isna().sum().sum()
 
 #@ 2
 # What fraction of all values in df are NA values?
 # (compute a number between 0 and 1)
-
+(df.isna().sum().sum())/(df.size)
 
 #@ 3
 # What fraction of the values in each column are NA?
 # Show only non-zero values, and sort the result by
 # decreasing value.
 # (compute a Pandas Series)
-
-
+df.isna().mean().loc[df.isna().mean() > 0].sort_values(ascending=False)
 
 #@ 4
 # Which columns contain more than 40% NA values?
 # (compute a NumPy array of the column names, sorted alphabetically)
-
+np.array(df.isna().mean().loc[df.isna().mean() > 0.4].index.sort_values(ascending=True))
 
 #@ 5
 # Compute a series that show the cumulative fraction of
@@ -77,12 +76,12 @@ df.info()
 # data in the next-most-NA column, etc.  The last values in
 # the series must be 1.0.
 # (compute a NumPy series)
-
+(df.isna().sum()/df.isna().sum().sum()).sort_values(ascending=False).cumsum()
 
 #@ 6
 # What fraction of the rows in df contain at least 2 NA values?
 # (compute a single number between 0 and 1)
-
+(df.isna().sum(axis=1).loc[df.isna().sum(axis=1) >= 2]).count()/len(df.index)
 
 #@ 7
 # There are other values in the dataset, besides NA, that
@@ -92,7 +91,7 @@ df.info()
 # Hint: to get only the 'object' type columns, consider
 # pandas.DataFrame.select_dtypes.
 # (compute a single number)
-
+(df.select_dtypes(exclude='number') == '').sum().sum()
 
 #@ 8
 # Would you expect contbr_employer to contain data that
@@ -102,7 +101,7 @@ df.info()
 # values, listed in decreasing order.  Do you think any
 # of the values represent missing values?
 # (compute a Series)
-
+df['contbr_employer'].value_counts().sort_values(ascending=False).iloc[:15]
 
 #@ 9
 # Repeat the previous problem, but this time create a data
@@ -114,7 +113,9 @@ df.info()
 # order.  The index of the returned series should range from
 # 0 to 14.  Then use pandas.DataFrame.apply() with this function.
 # (compute a DataFrame)
-
+def top_vals(s):
+    return pd.Series(s.value_counts().sort_values(ascending=False).iloc[:15].index)
+df[['contbr_employer', 'contbr_occupation', 'contbr_city']].apply(top_vals)
 
 #@ 10
 # Look carefully at the output of the last problem.  (Do this
@@ -127,7 +128,7 @@ df.info()
 # 'INFORMATION REQUESTED PER BEST EFFORTS' are placed with nan.
 # Use DataFrame.replace(), with option 'inplace=True'.
 # (write a pd.DataFrame.replace() statement)
-
+df.replace(['INFORMATION REQUESTED', 'INFORMATION REQUESTED PER BEST EFFORTS'], np.nan, True)
 
 #@ 11
 # Did you notice that 'NOT EMPLOYED' and 'NONE'
@@ -137,26 +138,26 @@ df.info()
 # with 'NOT EMPLOYED'.
 # Use Series.replace(), with option 'inplace=True'.
 # (write a pd.Series.replace() statement)
-
+df['contbr_employer'].replace(to_replace='NONE', value='NOT EMPLOYED', inplace=True)
 
 #@ 12
 # Lots of people are self-employed.  Compute a NumPy array of
 # all the values in 'contbr_occupation' that contain the string 'SELF'.
 # You may want to use pd.Series.str.contains().  Check out the 'na' option.
 # (compute a NumPy array)
-
+df['contbr_occupation'].loc[df['contbr_occupation'].str.contains('SELF', na=False)].unique()
 
 #@ 13
 # Update df to remove all columns containing at least 50% NA values.
 # Use DataFrame.dropna with the 'thresh' option
 # (write a pd.DataFrame.dropna() statement)
-
+df.dropna(axis=1,thresh=((len(df.index))/2),inplace=True)
 
 #@ 14
 # Column election_tp has a very small number of NA values.
 # Drop all *rows* of df for which election_tp is NA.
 # (write a pd.DataFrame.dropna() statement)
-
+df.dropna(subset=['election_tp'], inplace=True)
 
 #@ 15
 # What about bad zip values?  
@@ -164,11 +165,13 @@ df.info()
 # Hint: the regular expression that matches something that is not
 # a digit is '[^0-9]'  Consider using Series.str.contains() for this.
 # (compute a number)
+df['contbr_zip'].str.contains('[^0-9]').sum()
 
 
 #@ 16
 # What fraction of contb_receipt_amt values are less than 0?
 # (compute a number between 0 and 1)
+df['contb_receipt_amt'].loc[df['contb_receipt_amt'] < 0].count()/df['contb_receipt_amt'].count()
 
 
 #@ Here are some tests that look at your final data frame
@@ -178,7 +181,6 @@ df.info()
 df.info()
 
 (df['contbr_occupation'] == 'NONE').sum()
-
 
 
 
